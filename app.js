cd9561dc687d6c6339287681cfd43106d232dbb4
Boopesh Mahendran <boopeshmahendran@gmail.com>
2017-05-17 12:54:29 +0530

    Added dependencies and changed app.js

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
+const passport = require('passport')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
+mongoose.connect('mongodb://localhost:27017/pollit')
 
 const app = express()
 
+const apiRoutes = require('./routes/api')
+
 const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(cookieParser())
+app.use(session({
+  secret: 'abc',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use('/api', apiRoutes)
 
 app.get('/', function(req, res){
   res.send('hello world')
